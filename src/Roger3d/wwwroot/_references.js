55b7bcdf3013de3aba533ac9522ed04f34c3822d
rogeralsing <rogeralsing@gmail.com>
2015-11-23 15:09:24 +0100

    changed project type

--- src/Roger3d/wwwroot/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="js/data.js" />
-/// <reference path="js/site.js" />
-/// <reference path="js/vector.js" />
