6f593fe3d1ed95bc146fb5eb811709fa6e92b786
rogeralsing <rogeralsing@gmail.com>
2015-11-23 14:09:43 +0100

    cleanup

--- src/Roger3d/wwwroot/js/vector.ts
+++ src/Roger3d/wwwroot/js/vector.ts
@@ -435,44 +435,47 @@ class Graphics {
     }
 }
 
-setTimeout(() => {
+(() => {
+    var img = new Image();
+    img.onload = () => {
+        var loader = new FileLoader();
+        var obj = loader.loadFile(data, 11);
 
-    var loader = new FileLoader();
-    var obj = loader.loadFile(data, 11);
+        var element = <HTMLCanvasElement>document.getElementById("canvas1");
+        var c = element.getContext("2d");
 
-    var element = <HTMLCanvasElement>document.getElementById("canvas1");
-    var c = element.getContext("2d");
+        // read the width and height of the canvas
+        var width = element.width;
+        var height = element.height;
 
-    // read the width and height of the canvas
-    var width = element.width;
-    var height = element.height;
+        // create a new batch of pixels with the same
+        // dimensions as the image:
+        var canvas = c.createImageData(width, height);
 
-    // create a new batch of pixels with the same
-    // dimensions as the image:
-    var canvas = c.createImageData(width, height);
 
-    var img = <HTMLImageElement>document.getElementById("myTexture");
-    var textureCanvas = document.createElement("canvas");
-    textureCanvas.width = img.width;
-    textureCanvas.height = img.height;
-    var textureContext = textureCanvas.getContext("2d");
+        var textureCanvas = document.createElement("canvas");
+        textureCanvas.width = img.width;
+        textureCanvas.height = img.height;
+        var textureContext = textureCanvas.getContext("2d");
 
-    textureContext.drawImage(img, 0, 0, img.width, img.height);
-    var texture = textureContext.getImageData(0, 0, img.width, img.height);
+        textureContext.drawImage(img, 0, 0, img.width, img.height);
+        var texture = textureContext.getImageData(0, 0, img.width, img.height);
 
-    var g = new Graphics();
+        var g = new Graphics();
 
-    var xr = 0;
-    var yr = 0;
-    var zr = 0;
-    setInterval(() => {
-        c.clearRect(0, 0, canvas.width, canvas.height);
-        canvas = c.createImageData(width, height);
-        g.rotate(xr, yr, zr, obj);
-        xr += 0.01;
-        yr += 0.013;
-        zr += 0.02;
-        g.draw(obj, canvas, texture);
-        c.putImageData(canvas, 0, 0);
-    }, 10);
-}, 1500);
+        var xr = 0;
+        var yr = 0;
+        var zr = 0;
+        setInterval(() => {
+            c.clearRect(0, 0, canvas.width, canvas.height);
+            canvas = c.createImageData(width, height);
+            g.rotate(xr, yr, zr, obj);
+            xr += 0.01;
+            yr += 0.013;
+            zr += 0.02;
+            g.draw(obj, canvas, texture);
+            c.putImageData(canvas, 0, 0);
+        }, 10);
+    };
+    img.src = "/images/phong4.png";
+})();
